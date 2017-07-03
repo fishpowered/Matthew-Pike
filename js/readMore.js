d9791c553106a8b339f4cefecefef668f9158e53
Matt Pike <matt@fakeemailaddress.com>
2017-07-03 23:42:45 +0300

    Hooked up read more buttons

--- js/readMore.js
+++ js/readMore.js
@@ -1,3 +1,19 @@
-/**
- * Created by Matt Pike on 03/07/2017.
- */
+$(document).ready(function(){
+   $('.additionalInfo').hide();
+   $('button[data-type=readMoreButton],a[data-type=readMoreButton]').click(function(e){
+	   e.preventDefault();
+	   var targetId = '#'+$(this).data('context');
+	   var target = $(targetId);
+	   var button = $(this);
+	   if(target.data("isOpen")){
+		   target.data("isOpen", false);
+		   $(targetId+' > .additionalInfo').fadeOut(250);
+		   button.text(button.text().replace("Read less", "Read more"));
+	   }else{
+		   target.data("isOpen", true);
+		   $(targetId+' > .additionalInfo').fadeIn(250);
+		   button.text(button.text().replace("Read more", "Read less"));
+	   }
+	   target.focus();
+   });
+});
